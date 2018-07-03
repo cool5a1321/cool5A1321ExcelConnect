d5ffb7ee586adf521317ba44926adef9ded17004
caneja <chirag.aneja@prospectasoftware.com>
2018-07-03 11:35:43 +0530

    Removed sent comments

--- ConnektHub.Core/UserDetails.cs
+++ ConnektHub.Core/UserDetails.cs
@@ -3,7 +3,6 @@
     public class UserDetails
     {
 
-        //These are all getter and setters
         public bool isSSF { get; set; }
         public string roleId { get; set; }
         public string userId { get; set; }
